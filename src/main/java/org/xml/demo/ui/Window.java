9f967af314e83c8d1a32f88ea50e99be52c37ed8
SidelnikAndrej <andrej.sidelnik@gmail.com>
2019-05-26 11:00:17 +0300

    1

--- src/main/java/org/xml/demo/ui/Window.java
+++ src/main/java/org/xml/demo/ui/Window.java
@@ -4,13 +4,58 @@ import javax.swing.*;
 
 public class Window extends JFrame {
 
+    private String windowTitle;
+    private int windowHeight;
+    private int windowWidth;
+    private String workingDirectory;
+
+    public void init() {
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle(windowTitle);
+        setSize(windowWidth, windowHeight);
+    }
+
+    public static class WindowBuilder {
+
+        private Window instance = new Window();
+
+        public WindowBuilder windowTitle(String windowTitle) {
+            instance.windowTitle = windowTitle;
+            return this;
+        }
+
+        public WindowBuilder windowHeight(int windowHeight) {
+            instance.windowHeight = windowHeight;
+            return this;
+        }
+
+        public WindowBuilder windowWidth(int windowWidth) {
+            instance.windowWidth = windowWidth;
+            return this;
+        }
+
+        public WindowBuilder windowWorkingDirectory(String workingDirectory) {
+            instance.workingDirectory = workingDirectory;
+            return this;
+        }
+
+        public Window build() {
+
+            if (instance.workingDirectory == null) {
+                throw new RuntimeException("working directory is null!");
+            }
+            return instance;
+        }
+    }
+
     public static void main(String[] args) {
-        Window window = new Window();
-        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setTitle("Graphic editor v.1");
+        Window window = new Window.WindowBuilder().windowHeight(700).windowWidth(700).windowTitle("Test").build();
+        window.init();
+
+
         window.add(new GraphicArea());
 
-        window.setSize(700, 700);
         window.setVisible(true);
     }
 }
